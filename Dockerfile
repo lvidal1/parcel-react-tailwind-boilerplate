d6334b99b119fa7f22debe7d3bdea532ae5c3b02
lvidal1 <lvidal910@gmail.com>
2021-09-03 00:40:28 -0500

    Added tailwind config files & deps

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ ENV PATH /app/node_modules/.bin:$PATH
 
 # install and cache app dependencies
 COPY package.json /app/package.json
-RUN yarn install
+RUN npm install
 
 # start app
-CMD ["yarn", "start"]
+CMD ["npm", "start"]
--- src/index.js
+++ src/index.js
@@ -4,8 +4,18 @@ import ReactDOM from 'react-dom'
 const AppContainer = (props) => {
   const {text} = props;
   return (
-    <div className="container">
-      <p>{text}</p>
+    <div className="container-fluid">
+      <div className="flex bg-gray-100 py-24 justify-center">
+          <div className="p-12 text-center max-w-2xl">
+              <div className="md:text-3xl text-3xl font-bold">Want to discover more ?</div>
+              <div className="text-xl font-normal mt-4">{text}
+              </div>
+              <div className="mt-6 flex justify-center h-12 relative">
+                  <div className="flex shadow-md font-medium absolute py-2 px-4 text-green-100
+              cursor-pointer bg-green-600 rounded text-lg tr-mt  svelte-jqwywd">Try it now</div>
+              </div>
+          </div>
+      </div>
     </div>
   )
 }
